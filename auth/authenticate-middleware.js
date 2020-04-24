926df33e1bd316808d5e4d0e0da7128a5f667230
Alec Dye <alecdye@gmail.com>
2020-04-24 08:44:15 -0700

    Building auth middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,10 +1,22 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
-
-// Implement the authenticate middleware inside /auth/authenticate-middleware.js.
+const jwt = require("jsonwebtoken");
+const secrets = require("../api/secrets.js");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: "shall not pass!" });
+  // Postman -> { authorization: `token string` }
+  const token = req.headers.authorization;
+  const secret = secrets.jwtSecret;
+  // Decoding the submitted token for the correct user
+  if (token) {
+    jwt.verify(token, secret, (error, decodedToken) => {
+      // if something went wrong verifying the token (they don't match) throw an error
+      if (error) {
+        res.status(401).json({ you: "Access Denied!" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please provide user credentials" });
+  }
 };
